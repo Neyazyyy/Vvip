8a17595b501252f19ccc31a8897270c637956bff
Neyazyyy <neyazyyy@gmail.com>
2026-08-25 17:12:44 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    allowedHosts: true,
+  plugins: [react()],
+  build: {
+    target: 'esnext',
+    outDir: 'dist',
   },
-});
+})
